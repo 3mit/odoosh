961aeef2680545df392dfc6f22214f658427079c
victorDC <victor@3mit.dev>
2020-01-22 02:27:07 +0000

    first app commit

---
+++ angular_website/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class AngularWebsite(http.Controller):
+#     @http.route('/angular_website/angular_website/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/angular_website/angular_website/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('angular_website.listing', {
+#             'root': '/angular_website/angular_website',
+#             'objects': http.request.env['angular_website.angular_website'].search([]),
+#         })
+
+#     @http.route('/angular_website/angular_website/objects/<model("angular_website.angular_website"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('angular_website.object', {
+#             'object': obj
+#         })
