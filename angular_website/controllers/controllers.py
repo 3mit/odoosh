757d525e8eba5f536a523958832b73cfadf7a8c6
Victor del Carpio <victor@3mit.dev>
2020-01-22 21:20:01 +0000

    Prueba 1 desde template

--- angular_website/controllers/controllers.py
+++ angular_website/controllers/controllers.py
@@ -5,7 +5,7 @@ from odoo import http
 class AngularWebsite(http.Controller):
     @http.route('/', auth='public', website=True)
     def index(self, **kw):
-        return "Hello, world"
+        return http.request.render("angular_website.index", {})
 
 #     @http.route('/angular_website/angular_website/objects/', auth='public')
 #     def list(self, **kw):
