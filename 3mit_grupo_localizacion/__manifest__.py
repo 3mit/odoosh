09099eef60604053f9bf8252eeb9b9f5d10d46ad
mariagaby2018 <mariagaby2018@gmail.com>
2020-04-03 17:09:01 -0400

    Modulo necesario para permisos de grupo dependencia de 3mit_fiscal_requirements

---
+++ 3mit_grupo_localizacion/__manifest__.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+###########################################################################
+
+##############################################################################
+{
+    "name": "Localizacion Grupo",
+    "version": "1.0",
+    "author": "3mit",
+    "license": "AGPL-3",
+    "category": "grupos",
+    #"website": "",
+    "colaborador":"Yorman Pineda",
+    'depends': ['base'],
+
+    'demo': [
+    ],
+    "data": [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+    ],
+    'test': [
+
+    ],
+    "installable": True,
+    'application': True,
+}
